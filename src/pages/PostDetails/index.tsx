d1e53856fbbe07372ae8be4e67a372c6b77384d9
José Airton Neto <jose.neto.airton@gmail.com>
2025-07-22 14:02:50 -0300

    Post details page

--- src/pages/PostDetails/index.tsx
+++ src/pages/PostDetails/index.tsx
@@ -1,7 +1,44 @@
+import { Link } from 'react-router-dom'
+import { Content, Details, DetailsFooter, Info, PostDetailsContainer } from './styles'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleLeft, faCalendar, faComment } from '@fortawesome/free-solid-svg-icons'
+import { ExternalLink } from '../../components/ExternalLink/ExternalLink'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+
 export function PostDetails() {
   return (
-    <>
-      <h1>PostDetails</h1>
-    </>
+    <PostDetailsContainer>
+      <Details>
+        <div>
+          <Link to='/'>
+            <FontAwesomeIcon icon={faAngleLeft} />
+            Voltar
+          </Link>
+
+          <ExternalLink label='Ver no github' link='https://github.com/joseairtonneto' />
+        </div>
+
+        <h1>JavaScript data types and data structures</h1>
+
+        <DetailsFooter>
+          <Info>
+            <FontAwesomeIcon icon={faGithub} />
+            <span>cameronwll</span>
+          </Info>
+          <Info>
+            <FontAwesomeIcon icon={faCalendar} />
+            <span>Há 1 dia</span>
+          </Info>
+          <Info>
+            <FontAwesomeIcon icon={faComment} />
+            <span>5 comentários</span>
+          </Info>
+        </DetailsFooter>
+      </Details>
+
+      <Content>
+        teste
+      </Content>
+    </PostDetailsContainer>
   )
 }
---
+++ src/pages/PostDetails/styles.ts
@@ -0,0 +1,94 @@
+import styled from 'styled-components'
+
+export const PostDetailsContainer = styled.main`
+  width: 100%;
+  max-width: 54rem;
+
+  margin: -5.5rem auto 0;
+  z-index: 1;
+`
+
+export const Details = styled.header`
+  width: 100%;
+
+  background: ${({ theme }) => theme['base-profile']};
+  border-radius: 10px;
+  padding: 2rem;
+  box-shadow: 0px 2px 28px rgba(0, 0, 0, 0.2);
+
+  display: flex;
+  flex-direction: column;
+  gap: 1.25rem;
+
+  > div:first-child {
+    width: 100%;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    a {
+      width: fit-content;
+
+      color: ${({ theme }) => theme.blue};
+      border-bottom: 1px solid transparent;
+
+      text-decoration: none;
+      text-transform: uppercase;
+
+      font-weight: bold;
+      font-size: 0.75rem;
+
+      display: flex;
+      align-items: center;
+      gap: 0.5rem;
+
+      &:hover {
+        border-bottom: 1px solid ${({ theme }) => theme.blue};
+
+        transition: border-color 0.2s;
+      }
+    }
+  }
+
+  h1 {
+    color: ${({ theme }) => theme['base-title']};
+
+    font-size: 1.5rem;
+    font-weight: 700;
+    line-height: 1.3;
+  }
+`
+
+export const DetailsFooter = styled.div`
+  margin-top: -1rem;
+
+  display: flex;
+  gap: 1.5rem;
+`
+
+export const Info = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  svg {
+    color: ${({ theme }) => theme['base-label']};
+  }
+
+  span {
+    color: ${({ theme }) => theme['base-span']};
+  }
+`
+
+export const Content = styled.div`
+  padding: 2.5rem 2rem;
+
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+
+  p {
+    color: ${({ theme }) => theme['base-text']};
+  }
+`
